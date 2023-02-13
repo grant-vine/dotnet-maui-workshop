0e781a6006647dccb5510c969acff3320d845fc8
Grant Vine <grant@lolilabs.com>
2023-02-13 11:24:19 +0200

    Add Supabase and a Custom Auth Provider using local cache

--- Part 7 - Adding Supabase/MonkeyFinder/MauiProgram.cs
+++ Part 7 - Adding Supabase/MonkeyFinder/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Java.Net;
+using Microsoft.Extensions.Logging;
+using MonkeyFinder.Providers;
 using MonkeyFinder.Services;
 using MonkeyFinder.View;
 
@@ -19,7 +21,27 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
-    	builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+        var url = "https://nqdbztbjvfnswzuceysw.supabase.co";
+        var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xZGJ6dGJqdmZuc3d6dWNleXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYwMjUzNjMsImV4cCI6MTk5MTYwMTM2M30.1d4Zwa9OHSLWCDvlBOVevUC4AqTkhZ1IFsnqVzQCdzk";
+        
+        //builder.Services.AddSingleton<>();
+        builder.Services.AddSingleton<Supabase.Client>(
+			provider => new Supabase.Client(
+				url,
+				key,
+				new Supabase.SupabaseOptions
+				{
+					AutoRefreshToken = true,
+					AutoConnectRealtime = true,
+					PersistSession = true,
+					SessionHandler = new CustomSupabaseSessionHandler(
+						provider.GetRequiredService<ILogger<CustomSupabaseSessionHandler>>()
+					)
+				}
+			)
+		);
+
+        builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
 		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
 		builder.Services.AddSingleton<IMap>(Map.Default);
 
---
+++ Part 7 - Adding Supabase/MonkeyFinder/Providers/CustomSupabaseSessionHandler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Supabase.Interfaces;
+using Supabase.Gotrue;
+using Newtonsoft.Json;
+
+namespace MonkeyFinder.Providers
+{
+	public class CustomSupabaseSessionHandler : ISupabaseSessionHandler
+    {
+        private string supabaseCacheFilename = ".supabase.cache";
+        private readonly ILogger<CustomSupabaseSessionHandler> logger;
+
+        public CustomSupabaseSessionHandler(
+            ILogger<CustomSupabaseSessionHandler> logger
+        )
+        {
+            Console.WriteLine("------------------- CONSTRUCTOR -------------------");
+            this.logger = logger;
+        }
+
+        public Task<bool> SessionPersistor<TSession>(TSession session) where TSession : Session
+        {
+            try
+            {
+                var cacheDir = FileSystem.CacheDirectory;
+                var path = Path.Join(cacheDir, supabaseCacheFilename);
+                var str = JsonConvert.SerializeObject(session);
+
+                using (StreamWriter file = new StreamWriter(path))
+                {
+                    file.Write(str);
+                    file.Dispose();
+                    return Task.FromResult(true);
+
+                };
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine("Unable to write cache file.");
+                throw err;
+            }
+        }
+
+        public async Task<TSession?> SessionRetriever<TSession>() where TSession : Session
+        {
+            var tsc = new TaskCompletionSource<Session>();
+            try
+            {
+                var cacheDir = FileSystem.CacheDirectory;
+                var path = Path.Join(cacheDir, supabaseCacheFilename);
+
+                if (File.Exists(path))
+                {
+                    using (StreamReader file = new StreamReader(path))
+                    {
+                        var str = file.ReadToEnd();
+                        if (!String.IsNullOrEmpty(str))
+                            tsc.SetResult(JsonConvert.DeserializeObject<Session>(str));
+                        else
+                            tsc.SetResult(null);
+                        file.Dispose();
+                    };
+                }
+                else
+                {
+                    tsc.SetResult(null);
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Unable to read cache file.");
+                tsc.SetResult(null);
+            }
+            //return await localStorage.GetItemAsync<Session>(SESSION_KEY);
+            return (TSession?)await tsc.Task;
+
+        }
+
+        public Task<bool> SessionDestroyer()
+        {
+            try
+            {
+                var cacheDir = FileSystem.CacheDirectory;
+                var path = Path.Join(cacheDir, supabaseCacheFilename);
+                if (File.Exists(path))
+                    File.Delete(path);
+                return Task.FromResult(true);
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine("Unable to delete cache file.");
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
